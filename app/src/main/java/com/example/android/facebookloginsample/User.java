8d4075f5bf22758ca32bef87da081829916a795f
Nirav <niravnvk@gmail.com>
2015-04-23 12:20:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/facebookloginsample/User.java
@@ -0,0 +1,29 @@
+package com.example.android.facebookloginsample;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Android on 22-04-2015.
+ */
+public class User {
+
+    @SerializedName("")
+    public String name;
+    @SerializedName("")
+    public String password;
+    @SerializedName("")
+    public String email;
+    @SerializedName("")
+    public String phone;
+    @SerializedName("")
+    public String city;
+    @SerializedName("")
+    public String facebookID;
+    @SerializedName("")
+    public String profilePicture;
+    @SerializedName("")
+    public String gender;
+    @SerializedName("")
+    public String fbEmailID;
+
+}
